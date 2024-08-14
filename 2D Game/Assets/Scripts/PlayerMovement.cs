46af899be45bce12a30d05a6a18d239ed1f55c65
OliverMag321 <158530696+OliverMag321@users.noreply.github.com>
2024-08-14 13:25:56 +1000

    sasasasasasasasasasasasasasasasasasa

--- 2D Game/Assets/Scripts/PlayerMovement.cs
+++ 2D Game/Assets/Scripts/PlayerMovement.cs
@@ -66,7 +66,7 @@ private float jumpBufferCounter;
 
      private bool IsGrounded()
      {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 2f, groundLayer);
      }
 
      private void Dash()
